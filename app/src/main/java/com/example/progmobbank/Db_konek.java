e1c046e8ca6196aa8ec4163a40b1a07501f893c2
candrawijaya06 <hacciwijaya06@gmail.com>
2024-07-05 15:26:47 +0800

    Membuat Login Page

---
+++ app/src/main/java/com/example/progmobbank/Db_konek.java
@@ -0,0 +1,7 @@
+package com.example.progmobbank;
+
+public class Db_konek {
+    public static  String ip = "172.16.153.127:8080";
+
+    public static final String urlLogin = "http://"+ip+"/banksampah/api_login.php";
+}
--- app/src/main/java/com/example/progmobbank/LoginActivity.java
+++ app/src/main/java/com/example/progmobbank/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.progmobbank;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,6 +11,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -35,6 +45,9 @@ public class LoginActivity extends AppCompatActivity {
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
+                Log.d("LoginActivity", "Username: " + username);
+                Log.d("LoginActivity", "Password: " + password);
+
                 if (username.isEmpty() || password.isEmpty()) {
                     Toast.makeText(LoginActivity.this, "Harap isi semua kolom", Toast.LENGTH_SHORT).show();
                 } else if (!isValidEmail(username)) {
@@ -42,25 +55,14 @@ public class LoginActivity extends AppCompatActivity {
                 } else if (!isValidKataSandi(password)) {
                     Toast.makeText(LoginActivity.this, "Password harus menggunakan huruf dan angka minimal 8 karakter", Toast.LENGTH_SHORT).show();
                 } else {
-                    // Logika autentikasi di sini
-                    // Misalnya, lakukan pengecekan kredensial atau permintaan ke server
-
-                    Toast.makeText(LoginActivity.this, "Masuk berhasil", Toast.LENGTH_SHORT).show();
-
-                    // Arahkan ke BaseActivity
-                    Intent intent = new Intent(LoginActivity.this, BaseActivity.class);
-                    startActivity(intent);
-                    finish(); // Tutup LoginActivity agar pengguna tidak bisa kembali dengan menekan tombol kembali
+                    new LoginTask().execute(username, password);
                 }
             }
         });
 
-
-
         forgotPasswordTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Tambahkan logika untuk lupa kata sandi
                 Toast.makeText(LoginActivity.this, "Lupa Kata Sandi diklik", Toast.LENGTH_SHORT).show();
             }
         });
@@ -68,12 +70,10 @@ public class LoginActivity extends AppCompatActivity {
         createAccountTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Pindah ke halaman registrasi
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
-
     }
 
     private boolean isValidEmail(String email) {
@@ -82,5 +82,63 @@ public class LoginActivity extends AppCompatActivity {
 
     private boolean isValidKataSandi(String kataSandi) {
         return kataSandi.matches("^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$");
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String username = params[0];
+            String password = params[1];
+            String response = "";
+
+            try {
+                String apiUrl = Db_konek.urlLogin;
+                String queryParams = "?nama=" + username + "&password=" + password;
+                URL url = new URL(apiUrl + queryParams);
+
+                Log.d("LoginTask", "URL: " + url.toString());
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                reader.close();
+                response = sb.toString();
+                Log.d("LoginTask", "Server Response: " + response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            handleLoginResponse(result);
+        }
+    }
+
+    private void handleLoginResponse(String response) {
+        try {
+            Log.d("LoginActivity", "Response: " + response);
+            JSONObject jsonResponse = new JSONObject(response);
+            String status = jsonResponse.getString("status");
+
+            if (status.equals("success")) {
+                Toast.makeText(LoginActivity.this, jsonResponse.getString("message"), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, BaseActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(LoginActivity.this, jsonResponse.getString("message"), Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(LoginActivity.this, "Error parsing JSON response", Toast.LENGTH_SHORT).show();
+        }
     }
 }
